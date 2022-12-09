406619e02e9cd01ad00ea02e5b50c5a80ba807b2
Egonyu <danielsegonyu@outlok.com>
2022-12-09 15:35:22 -0800

    Task 4

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,15 @@
-#!/bin/bash
+#include <stdio.h>
 
+/*
+ * main function
+ *
+ * Description: Program prints to the screen
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n";
+	return (0);
+}
