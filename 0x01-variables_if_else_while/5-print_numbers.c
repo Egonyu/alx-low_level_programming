5d6dd95b3e351b8ef6c25000725157bc176daffe
Egonyu <danielsegonyu@outlok.com>
2022-12-12 11:31:00 -0800

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main - program to print numbers os base 10 from 0-9
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		printf("%d", a);
+	}
+
+	printf("\n");
+
+	return (0);
+}
