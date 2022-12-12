ef443e7e438f4954da645941e9f6254552ed9983
Egonyu <danielsegonyu@outlok.com>
2022-12-12 12:16:36 -0800

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ *main - program to print all possible combinations of single-digit numbers
+ *
+ *Return: Always 0 (success)
+ */
+int mainn(void)
+
+{
+	int num1, num2;
+
+	for (num = 0; num1 <= 9; num1++)
+	{
+		for (num2 = num1 + 1; num2 <= 9; num2++)
+		{
+			putchar(num1 + '0');
+			putchar(',');
+			putchar(' ');
+			putchar(num2 + '0');
+			puthcar('\n');
+		}
+	}
+
+	return (0);
+}
