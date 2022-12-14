9cb9151f6647d9d1407f7d1961263593af542f1e
Egonyu <danielsegonyu@outlok.com>
2022-12-13 17:14:04 -0800

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *print_alphabet - Program to print alphabet in lowercases
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+
+	_putchar('\n');
+}
