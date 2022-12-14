2059a59dc8b184d53a6c1f32b5c25743c88de1a3
Egonyu <danielsegonyu@outlok.com>
2022-12-13 17:59:34 -0800

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main"
+
+/**
+ *_abs - computer ablsolute value of an integer
+ *@i: The integer to be computed
+ *Return: The absoluter value
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+		return (i);
+	else
+		return (-i);
+}
