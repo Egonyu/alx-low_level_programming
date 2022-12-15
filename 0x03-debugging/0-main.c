7f67f6f3aba64a66059cdbc935230aab797e3157
Egonyu <danielsegonyu@outlok.com>
2022-12-15 15:17:17 -0800

    Task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *main - program tests if integer is positive or negative
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 3;
+
+	positive_or_negative(i);
+
+	return (0);
+}
