ba62f8d39cbe03878e40233b0a6d76615c556cd6
Egonyu <danielsegonyu@outlok.com>
2022-12-19 11:15:24 -0800

    Task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,11 +8,16 @@
 
 void print_rev(char *s)
 {
-	int strlen = _strlen(s);
+    // find the length of the string
+    int length = _strlen(s);
 
-	for (int i = strlen - 1; i >= 0; i--)
-	{
-		putchar(s[i]);
-	}
-	putchar('\n');
+    // print the characters of the string in reverse order
+    for (int i = length - 1; i >= 0; i--)
+    {
+        putchar(s[i]);
+    }
+
+    // print a newline character
+    putchar('\n');
 }
+
