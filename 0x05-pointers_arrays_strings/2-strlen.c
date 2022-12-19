757b7fdb7a5d09232125b08ae6b131dcb592b0fb
Egonyu <danielsegonyu@outlok.com>
2022-12-19 10:53:05 -0800

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "dan.h"
+
+/**
+ *int _strlen - program that return legnth of a string
+ *@s: string to be measured
+ *Return: length
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+
+		s++;
+	}
+
+	return length;
+}
