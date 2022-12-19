af5e58d9fb63e8bd6897c08a546e86900a8632fc
Egonyu <danielsegonyu@outlok.com>
2022-12-19 10:40:19 -0800

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "dan.h"
+
+/**
+ *reset_to_98 - take a pointer to an int as parameter and updates
+ *the value it points to is 98
+ *@n: point to an int
+ *Return: 0
+ */
+
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
