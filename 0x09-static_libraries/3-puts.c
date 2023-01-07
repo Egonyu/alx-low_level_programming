848722f105540da54f24a0ded24ee7b8e15e34ff
Egonyu <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@github.com>
2023-01-07 03:09:46 -0800

    Task 0

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,22 +1,20 @@
 #include "main.h"
-#include<stdio.h>
 
 /**
- * _puts - Prints a string to stdout
- * @str: Input string
- */
+* _puts - prints a string, followed by a new line,
+* @str: pointer to the string to print
+* Return: void
+*/
+
 void _puts(char *str)
 {
-	printf("%s\n", str);
-}
+	int i = 0;
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	_puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
 }
